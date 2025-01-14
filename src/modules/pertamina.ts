9d5c7cd97e546f80f0e2ce51e5b611bb9e0a17d4
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2025-01-14 16:24:24 +0700

    Sandbox

--- src/modules/pertamina.ts
+++ src/modules/pertamina.ts
@@ -36,7 +36,11 @@ export class Pertamina {
 
     console.log(`[+] Login using ${this.username}...`);
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox"],
+      executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
+      headless: false,
+    });
 
     const page = await browser.newPage();
 
